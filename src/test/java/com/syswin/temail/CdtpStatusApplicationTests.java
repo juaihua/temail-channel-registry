a036e845525dc262ac8c288733503e34e6fda743
鞠爱华 <juaihua@syswin.com>
2018-08-13 15:31:01 +0800

    项目初始化

---
+++ src/test/java/com/syswin/temail/CdtpStatusApplicationTests.java
@@ -0,0 +1,16 @@
+package com.syswin.temail;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CdtpStatusApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
