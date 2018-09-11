55c68dcc5cccdc05800a8a5ac26bc6db860ab4de
鞠爱华 <juaihua@syswin.com>
2018-09-11 21:10:53 +0800

    配置使用disconf管理

--- temail-channel-registry-server/src/main/resources/startup-channel-server.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-java -jar -Dlog.path.prefix=/opt/apps/logs  temail-channel-server-1.0.0-SNAPSHOT.jar --spring.profiles.active=frontend 2>&1 1 > /dev/null &
-
--- temail-channel-registry-server/src/main/resources/stop-channel-server.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ps -ef|grep temail-channel | grep -v "grep" | awk '{print $2}' | xargs kill -9
