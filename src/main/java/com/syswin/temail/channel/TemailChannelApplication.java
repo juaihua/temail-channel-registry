23e3fa036f2b1d5b407d7ffd4aff5ef3809f2c2e
鞠爱华 <600852@syswin.com>
2019-03-15 12:38:22 +0800

    增加war包的启动配置

--- src/main/java/com/syswin/temail/channel/TemailChannelApplication.java
+++ src/main/java/com/syswin/temail/channel/TemailChannelApplication.java
@@ -2,9 +2,10 @@ package com.syswin.temail.channel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TemailChannelApplication {
+public class TemailChannelApplication  extends SpringBootServletInitializer {
   public static void main(String[] args) {
     SpringApplication.run(TemailChannelApplication.class, args);
   }
